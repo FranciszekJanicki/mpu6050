4ffa534c02b2e46d4c8de1af8ad11a4e2f90403e
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-25 19:58:28 +0200

    changes

--- mpu6050_dmp.c
+++ mpu6050_dmp.c
@@ -23,15 +23,16 @@
 //     mpu6050_set_clock_source(mpu6050, Clock::PLL_ZGYRO);
 //     mpu6050_set_int_dmp_enabled(mpu6050, true);
 //     mpu6050_set_int_fifo_overflow_enabled(mpu6050, true);
-//     mpu6050_set_sampling_rate(mpu6050, 200, DLPF::BW_42); // 1 / (1 + 4) = 200 Hz
-//     mpu6050_set_external_frame_sync(mpu6050, ExtSync::TEMP_OUT_L);
+//     mpu6050_set_sampling_rate(mpu6050, 200, DLPF::BW_42); // 1 / (1 + 4) =
+//     200 Hz mpu6050_set_external_frame_sync(mpu6050, ExtSync::TEMP_OUT_L);
 //     mpu6050_set_dlpf_mode(mpu6050, DLPF::BW_42);
 //     mpu6050_set_full_scale_gyro_range(mpu6050, GYRO_RANGE);
-//     mpu6050_write_memory_block(mpu6050, mpu6050_img, sizeof(mpu6050_img), 0x00, 0x00);
+//     mpu6050_write_memory_block(mpu6050, mpu6050_img, sizeof(mpu6050_img),
+//     0x00, 0x00);
 
 //     uint8_t mpu6050_update[] = {0x00, 0x01};
-//     mpu6050_write_memory_block(mpu6050, dmp_update, sizeof(dmp_update), 0x02, 0x16);
-//     mpu6050_set_dmp_config1(mpu6050, 0x03);
+//     mpu6050_write_memory_block(mpu6050, dmp_update, sizeof(dmp_update), 0x02,
+//     0x16); mpu6050_set_dmp_config1(mpu6050, 0x03);
 //     mpu6050_set_dmp_config2(mpu6050, 0x00);
 //     mpu6050_set_otp_bank_valid(mpu6050, false);
 
@@ -47,7 +48,8 @@
 //     mpu6050_set_dmp_enabled(mpu6050, true);
 // }
 
-// mpu6050_err_t mpu6050_get_dmp_packet(mpu6050_t const* mpu6050, mpu6050_dmp_packet_t* packet)
+// mpu6050_err_t mpu6050_get_dmp_packet(mpu6050_t const* mpu6050,
+// mpu6050_dmp_packet_t* packet)
 // {
 //     mpu6050_dmp_packet_t packet;
 
@@ -104,7 +106,7 @@
 //         data |= 0x20;
 //     if (prefetch_enabled)
 //         data |= 0x40;
-//     mpu6050_bus_write(mpu6050, MPU6050_REG_ADDRESS_BANK_SEL, data);
+//     mpu6050_bus_write_data(mpu6050, MPU6050_REG_ADDRESS_BANK_SEL, data);
 // }
 
 // mpu6050_err_t mpu6050_read_dmp_memory_block(mpu6050_t const* mpu6050,
@@ -126,9 +128,8 @@
 //             chunk_size = 256 - address;
 //         }
 
-//         mpu6050_bus_read(mpu6050, MPU6050_REG_ADDRESS_MEM_R_W, data + i, chunk_size);
-//         i += chunk_size;
-//         address += chunk_size;
+//         mpu6050_bus_read_data(mpu6050, MPU6050_REG_ADDRESS_MEM_R_W, data + i,
+//         chunk_size); i += chunk_size; address += chunk_size;
 
 //         if (i < data_size) {
 //             if (address == 0) {
@@ -159,9 +160,8 @@
 //             chunk_size = 256 - address;
 //         }
 
-//         mpu6050_bus_write(mpu6050, MPU6050_REG_ADDRESS_MEM_R_W, data + i, chunk_size);
-//         i += chunk_size;
-//         address += chunk_size;
+//         mpu6050_bus_write_data(mpu6050, MPU6050_REG_ADDRESS_MEM_R_W, data +
+//         i, chunk_size); i += chunk_size; address += chunk_size;
 
 //         if (i < data_size) {
 //             if (address == 0) {
@@ -183,11 +183,12 @@
 //         uint8_t length = data[i++];
 
 //         if (length > 0) {
-//             mpu6050_write_memory_block(mpu6050, data + i, length, bank, offset);
-//             i += length;
+//             mpu6050_write_memory_block(mpu6050, data + i, length, bank,
+//             offset); i += length;
 //         } else {
 //             if (data[i++] == 0x01) {
-//                 mpu6050_bus_write(mpu6050, MPU6050_REG_ADDRESS_INT_ENABLE, 0x32);
+//                 mpu6050_bus_write_data(mpu6050,
+//                 MPU6050_REG_ADDRESS_INT_ENABLE, 0x32);
 //             }
 //         }
 //     }
